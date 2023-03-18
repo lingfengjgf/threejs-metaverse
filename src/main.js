0b4a4c8e92ed7a5d0857b5c0cd7c5d9e88435413
lingfengjgf <lingfengjgf@163.com>
2023-03-18 18:50:01 +0800

    添加阴影

--- src/main.js
+++ src/main.js
@@ -7,11 +7,13 @@ console.log(THREE);
 // 创建场景
 const scene = new THREE.Scene();
 // 创建相机
-const camera = new THREE.PerspectiveCamera(45,window.innerWidth/window.innerHeight,0.01,200);
+const camera = new THREE.PerspectiveCamera(80,window.innerWidth/window.innerHeight,0.01,200);
 // 创建渲染器
 const renderer = new THREE.WebGLRenderer();
 // 打开renderer阴影
-// renderer.shadowMap.enabled = true;
+renderer.shadowMap.enabled = true;
+// 阴影贴图类型
+renderer.shadowMap.type = THREE.PCFShadowMap;
 renderer.setSize(window.innerWidth,window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
@@ -25,8 +27,29 @@ const ambientLight = new THREE.AmbientLight(0xffffff,0.6);
 scene.add(ambientLight);
 
 // 方向光
-const directionLight = new THREE.DirectionalLight(0xffffff,0.4);
+const directionLight = new THREE.DirectionalLight(0xffffff,0.6);
 scene.add(directionLight);
+directionLight.position.set(40, 40, 30);
+directionLight.castShadow = true;
+
+const SHADOW_DISTANCE = 50;
+directionLight.shadow.camera.near = 0.1;
+directionLight.shadow.camera.far = SHADOW_DISTANCE * 2;
+directionLight.shadow.camera.left = -SHADOW_DISTANCE;
+directionLight.shadow.camera.right = SHADOW_DISTANCE;
+directionLight.shadow.camera.top = SHADOW_DISTANCE;
+directionLight.shadow.camera.bottom = -SHADOW_DISTANCE;
+directionLight.shadow.bias = -0.001;
+directionLight.shadow.mapSize.width = 4096;
+directionLight.shadow.mapSize.height = 4096;
+
+// 方向光辅助体
+const dirHelper = new THREE.DirectionalLightHelper(directionLight, 5);
+scene.add(dirHelper);
+
+// 相机辅助体
+const camHelper = new THREE.CameraHelper(directionLight.shadow.camera);
+scene.add(camHelper);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
@@ -38,6 +61,12 @@ const controls = new OrbitControls(camera, renderer.domElement);
 const gltfLoader = new GLTFLoader();
 gltfLoader.load('scene.glb',(gltf)=>{
   scene.add(gltf.scene);
+  gltf.scene.traverse(child=>{
+    if(child.type === 'Mesh'){
+      child.castShadow = true;
+      child.receiveShadow = true;
+    }
+  })
 })
 
 // 帧循环
